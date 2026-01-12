38e92b9402c6032fc2176a5b56e2f839e9f2e2aa
Eduart Torba <eduart.torba@loclaweb.it>
2026-01-12 11:06:59 +0100

    stripe payment

--- stripe_initialization.php
+++ stripe_initialization.php
@@ -1,8 +1,8 @@
 <?php
 require_once('vendor/autoload.php');
 
-$private_key = "sk_test_51QdZaOIA6j8Agjdo5GJTLy0SpIDPNae1bPpzug7GTqcDIUlNR7CKK1ojTH9gGYRe0uEHUjbnpIDGSKukUVTONETg00wfXvfRpk";
-$public_key = "pk_test_51QdZaOIA6j8AgjdoONN2YmHKTojcogE82ZcF8ntm0l1YwdZNUKNnlDgxb62vZ7IBVbS1NfyGQoRNxjWn6o0bvJxE00alHhiENc";
+$private_key = "";
+$public_key = "";
 $stripe_account = "Test";
 $businessName = "Test";
 $company_name = "Test";
